6e8da5ed52d354c0de00f816c1f60e48e5bec124
as6620 <as6620@bs.amalnet.k12.il>
2024-05-06 15:01:17 +0300

    Initial commit

---
+++ app/src/main/java/com/example/radiogroup/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.radiogroup;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.text.Layout;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.Switch;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    ConstraintLayout TL;
+    Switch swtch;
+    RadioButton rbRed, rbYellow, rbGreen, rbBlue;
+    Button btnChange;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initViews();
+    }
+
+    private void initViews() {
+        TL = findViewById(R.id.TL);
+
+        swtch = (Switch) findViewById(R.id.swtch);
+
+        rbRed = (RadioButton) findViewById(R.id.rbRed);
+        rbYellow = (RadioButton) findViewById(R.id.rbYellow);
+        rbBlue = (RadioButton) findViewById(R.id.rbBlue);
+        rbGreen = (RadioButton) findViewById(R.id.rbGreen);
+
+        btnChange = (Button) findViewById(R.id.btnChange);
+    }
+
+
+    public void goChange(View view) {
+        
+    }
+}
